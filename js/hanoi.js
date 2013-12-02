8bef9d615eca4069abbd00fd63c4c3659c95ebcc
asmallorange <jeanalesi007@gmail.com>
2013-12-02 19:46:07 -0300

    adding comments

--- js/hanoi.js
+++ js/hanoi.js
@@ -1,132 +1,188 @@
-const MAX_SIZE = 7;
-	var number_of_moves;
-	var minimum_number_of_moves = [0, 1 , 3, 7, 15, 31, 63, 127];
-	image_objects = new Array();
-	var obj = null;
+// hanoi.js
+
+
+/**
+ * 
+ * This JavaScript file handles the logic required to run the Towers of Hanoi on a webpage.
+ * 
+ * @author Antonio Recalde
+ * @version 12/01/2013
+ */
+
+const MAX_SIZE = 7;									// This is the maximum number of blocks the game can start with.
+var number_of_moves;								// This variable will store the number of moves the user has made so far.
+var min_moves = [0, 1 , 3, 7, 15, 31, 63, 127];		// Holds value of minimum number of moves, in which it's possible to win the game
+image_objects = new Array();						// This variable will hold all the image objects in the game
+
+
+
+/*---------------------------------------------------------------------------
+ * 
+ * This for loop will retrieve references in memory to the objects used to play this game. The purpose of this is that when restarting
+ * the game, all blocks in all positions shall be deleted... thus, we need to keep a pointer to those objects so we can reload the towers in 
+ * their initial position
+ * 
+ *---------------------------------------------------------------------------*/
+for (var i = 0; i < MAX_SIZE; i++)
+	image_objects[i] = $("#level" + i).get();
+
+
+/*
+ * Starting game object, and saving a reference to in in a variable $game. When the game gets restarted, a new TowersOfHanoi object will be created
+ * and the previous object (from the old game) shall be de-refereced and set for garbage collection. Thus, only one instance of the game
+ * will be running at a time. 
+ *  
+ */ 
+var $game = new TowersOfHanoi("start");
 	
-	for (var i = 0; i < MAX_SIZE; i++)
-		image_objects[i] = $("#level" + i).get();
+
+/**
+ * 	This function runs the game. Makes the moves based on the information acquired via the listeners  draggable 
+ * 	and droppable. 
+ * 
+ * @param size
+ * @returns
+ */
+function TowersOfHanoi(size)
+{	
+	var obj = null;								// this will be used to refer to the object currently being dragged outside the drag function.
+	var game_end = false;						// this boolean indicates whether end of game code should be executed or not.
+	const GAME_SIZE = size == "start" ? MAX_SIZE : size;
+	const NUM_TOWERS = 3;				
+	var origin = null;							// keeps track of the tower a moving block originated from
+	
+	number_of_moves = 0;
+	
+	/*------------------------------------------------------
+	 * 
+	 * These "tower" arrays will keep track of the coordinates of each element.
+	 * They will store the id value of the objects that occupy each position.
+	 * 
+	 * These arrays allow for the retrieval of object id's by position
+	 * 
+	 *-----------------------------------------------------*/
+	tower = [];
+	tower["A"] = new Array();
+	tower["B"] = new Array();
+	tower["C"] = new Array();
 	
 	
-	var $game = new TowersOfHanoi("start");
+	var pos = [];								// allows for the retrieval of coordinates by the object they hold
 	
-	function TowersOfHanoi(size)
-	{	
-		var game_end = false;
-		const GAME_SIZE = size == "start" ? MAX_SIZE : size;
-		const NUM_TOWERS = 3;
-		var origin = null;
-		
-		number_of_moves = 0;
-		
-		tower = [];
-		tower["A"] = new Array();
-		tower["B"] = new Array();
-		tower["C"] = new Array();
-		
-		var pos = [];				// store the last position of each block in array pos
-		
-		initialize();
-		
-		$(function() {
-		    $( '.draggable' ).draggable({
-		    	containment: '#game', 
-		    	revert: "invalid",
-		    	start: function() {
-		    		if (game_end)
-		    			return false;
-		    		
-		    		$('#' + pos[this.id][0]).droppable("disable");		// cannot drop in origin tower
-		    		
-		    		// do not drop over smaller....
-		    		for (var t in tower)
-		    		{
-		    			top_level = tower[t].length - 1;
-		    			
-		    			if (this.id <= tower[t][top_level]) // if this is not bigger than peak ABC, disable draggable
-		    				$("#" + t).droppable("disable");
-		    		}
-		    		
-		    		if (!(this.id == tower["A"][ (tower["A"].length - 1) ] || this.id == tower["B"][(tower["B"].length - 1)] || this.id == tower["C"][(tower["C"].length - 1)]))
-		    			return false;
-		    		
-		    		obj = this;
-		    	}, // end start: function
-		    	
-		    	stop: function() {
-		    		for (var t in tower)				// restore droppable in all towers
-		    			$('#' + t).droppable("enable");
-		    		
-		    	}
-		    }
-	 	);
-		    $( ".droppable" ).droppable({
-		    	activeClass: "active",
-		    	drop: function() {
-		    		processMove(this.id);
-		    	}
-		    });
-		});
-			
-		function initialize()
+	
+	initialize();								
+	
+	
+	$(function() {
+	    $( '.draggable' ).draggable({
+	    	containment: '#game', 
+	    	revert: "invalid",
+	    	start: function() {
+	    		if (game_end)					// disables movement of blocks when game has been finished
+	    			return false;
+	    		
+	    		// Ensures that the user cannot drop in the same tower it came from. Instead, it simply reverts back.
+	    		$('#' + pos[this.id][0]).droppable("disable");		
+	    		
+	    		
+	    		// The following for loop prevents that a block be dropped on top of a smaller block.
+	    		for (var t in tower)
+	    		{
+	    			top_level = tower[t].length - 1;
+	    			if (this.id <= tower[t][top_level]) 			// compares ids.. if the objects id is heigher than the target, the drop is disabled
+	    				$("#" + t).droppable("disable");			
+	    		}
+	    		
+	    		// Ensures that the block is draggable only if its in the top of its tower.
+	    		if (!(this.id == tower["A"][ (tower["A"].length - 1) ] || this.id == tower["B"][(tower["B"].length - 1)] || this.id == tower["C"][(tower["C"].length - 1)]))
+	    			return false;
+	    		
+	    		obj = this;							// save reference to the object being dragged
+	    	}, // end start: function
+	    	
+	    	stop: function() {
+	    		for (var t in tower)				// restores droppable to all towers once the move is finalized
+	    			$('#' + t).droppable("enable");
+	    		
+	    	}
+	    }
+ 	);
+	    $( ".droppable" ).droppable({				
+	    	activeClass: "active",
+	    	drop: function() {
+	    		processMove(this.id);
+	    	}
+	    });
+	});
+	
+	/**
+	 * This function sets up the game in its initial position
+	 */
+	function initialize()
+	{
+		/*
+		 * If the game has been restarted, delete all blocks from all cells.
+		 */
+		if (size != "start")						
 		{
-			if (size != "start")
+			for (var index in tower)
 			{
-				for (var letter in tower)
-				{
-					for (var i = 0; i < MAX_SIZE; i++)
-						$("#" + letter + i).html("");
-				}
-			}
-			
-			
-			for (var i = 0; i < (GAME_SIZE); i++)
-			{
-					pos['level' + i] = ['A', i, 'A'+i];		// [0] current tower, [1] the current level.
-					tower["A"][i] = 'level' + i;
-					
-					if (size !=  "number")
-						$("#A" + i).html(image_objects[i]);
-			}
-			
-			$("#min").html(minimum_number_of_moves[GAME_SIZE]);
-			$("#curr").html(number_of_moves);
-		}
-			
-		function processMove(target)
-		{
-			origin = pos[obj.id][0];
-			addToTower(obj, target);
-			
-			$("#curr").html(++number_of_moves);
-			
-			if (tower["C"].length == GAME_SIZE)
-			{
-				game_end = true;
-				alert('\tYOU WIN!!\n' +
-						'To play again click the "New Game" button.\nYou finished this puzzle in ' + number_of_moves + ' move(s).\n' + 
-						'Best possible: ' + minimum_number_of_moves[GAME_SIZE] + '.');
+				for (var i = 0; i < MAX_SIZE; i++)
+					$("#" + index + i).html("");
 			}
 		}
 		
-		function addToTower(obj, target)
-		{
-			var height;
-			// here
-			height = tower[target].length;		
-			
-			destination_row = "" + target + height;
-			$('#' + obj.id).css({'left': 0, 'top': 0});			// ensures image stays inside of <td>
-			$('#' + destination_row).html(obj);
-			
 		
-			pos[obj.id] = [target, height, destination_row];
-			tower[target][height] = tower[origin].pop();
-			
-			
-			pos[obj.id] = [target, height, destination_row];		// update the position coordinates of the element just dropped
+		/*
+		 * Place all the blocks in their initial position. Only place as many blocks as the game size.
+		 */
+		for (var i = 0; i < (GAME_SIZE); i++)
+		{
+				pos['level' + i] = ['A', i, 'A'+i];				// save the position of the object given the object id.
+				tower["A"][i] = 'level' + i;					// now save the id of each object in relation to its position.
+				
+				if (size !=  "start")
+					$("#A" + i).html(image_objects[i]);			// if the game is restarting, reload the cells with blocks depending on GAME_SIZE
 		}
-	}	
+		
+		$("#min").html(min_moves[GAME_SIZE]);
+		$("#curr").html(number_of_moves);
+	}
+		
+	function processMove(target)
+	{
+		origin = pos[obj.id][0];
+		addToTower(obj, target);
+		
+		$("#curr").html(++number_of_moves);
+		
+		if (tower["C"].length == GAME_SIZE)
+		{
+			game_end = true;
+			alert('\tYOU WIN!!\n' +
+					'To play again click the "New Game" button.\nYou finished this puzzle in ' + number_of_moves + ' move(s).\n' + 
+					'Best possible: ' + min_moves[GAME_SIZE] + '.');
+		}
+	}
+	
+	function addToTower(obj, target)
+	{
+		var height;
+		// here
+		height = tower[target].length;		
+		
+		destination_row = "" + target + height;
+		$('#' + obj.id).css({'left': 0, 'top': 0});			// ensures image stays inside of <td>
+		$('#' + destination_row).html(obj);
+		
+	
+		pos[obj.id] = [target, height, destination_row];
+		tower[target][height] = tower[origin].pop();
+		
+		
+		pos[obj.id] = [target, height, destination_row];		// update the position coordinates of the element just dropped
+	}
+}	
 	
 	$(document).ready(function(){ 
 	    $("button").click(function () {
